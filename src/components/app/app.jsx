ee9c4097ebb46969499f1c16a283f2524e623caa
Лончук Вячеслав Петрович <v.lonchuk@hq.qiwi.com>
2019-10-19 23:42:40 +0300

    module2-task1

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,8 +2,27 @@ import React from 'react';
 import Main from './../main/main.jsx';
 
 const App = () => {
+  const properties = [
+    {
+      id: `prop-1`,
+      caption: `Beautiful & luxurious apartment at great location`,
+    },
+    {
+      id: `prop-2`,
+      caption: `Wood and stone place`,
+    },
+    {
+      id: `prop-3`,
+      caption: `Canal View Prinsengracht`,
+    },
+    {
+      id: `prop-4`,
+      caption: `Nice, cozy, warm big bed apartment`,
+    },
+  ];
+
   return <section>
-    <Main />
+    <Main properties={properties} />
   </section>;
 };
 
