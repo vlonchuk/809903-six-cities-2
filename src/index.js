45e11288a30eb00a7cb3c42ffb820d955f9663b2
vlonchuk <vlonchuk@gmail.com>
2019-12-04 20:44:49 +0300

    added react-dev-tools

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import reducer from './reducer/reducer.js';
 import {Provider} from 'react-redux';
 
 const init = () => {
-  const store = createStore(reducer);
+  const store = createStore(reducer,
+      window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f);
   ReactDOM.render(
       <Provider store={store}>
         <App />
